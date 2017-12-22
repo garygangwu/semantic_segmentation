7b05db44eac48f7e59b21f8ab73913ffc82ef030
Gary Wu <mr.gang.wu@gmail.com>
2017-12-22 05:20:43 +0000

    apply is_training to helper.save_inference_samples

--- inference.py
+++ inference.py
@@ -12,6 +12,7 @@ def main():
   input_image_tensor_name = 'image_input:0'
   keep_prob_tensor_name = 'keep_prob:0'
   logits_tensor_name = 'new_logits:0'
+  is_training_name = 'is_training:0'
 
   with tf.Session() as sess:
     tf.saved_model.loader.load(sess, ['test'], MODEL_PATH)
@@ -20,8 +21,13 @@ def main():
     input_image = graph.get_tensor_by_name(input_image_tensor_name)
     keep_prob = graph.get_tensor_by_name(keep_prob_tensor_name)
     logits = graph.get_tensor_by_name(logits_tensor_name)
+    is_training = graph.get_tensor_by_name(is_training_name)
 
-    helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+    helper.save_inference_samples(
+      runs_dir, data_dir, 
+      sess,
+      image_shape,
+      logits, keep_prob, is_training, input_image)
 
 if __name__ == '__main__':
     main()
