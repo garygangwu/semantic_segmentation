c7153bb937a60d2dc911f18d27f9aa224072d159
Gary Wu <gary.wu@airbnb.com>
2017-12-21 18:25:24 -0800

    normalize the input

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('data_dir', './data', 'data fold location')
 flags.DEFINE_integer('epochs', 50, 'number of epochs for training')
-flags.DEFINE_integer('batch_size', 5, 'batch size per training')
+flags.DEFINE_integer('batch_size', 10, 'batch size per training')
 flags.DEFINE_float('learning_rate', 0.001, 'learning rate')
 IMAGE_SHAPE = (160, 576)
 USE_ORIGINAL_NUM_FILTERS = True
@@ -55,7 +55,7 @@ def load_vgg(sess, vgg_path):
 tests.test_load_vgg(load_vgg, tf)
 
 
-def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
+def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes, is_training):
     """
     Create the layers for a fully convolutional network.  Build skip-layers using the vgg layers.
     :param vgg_layer7_out: TF Tensor for VGG Layer 3 output
@@ -78,6 +78,9 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
                                                padding='same', activation=tf.nn.relu,
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
                                                name='new_dconv_layer_7')
+    dconv_layer_7 = tf.layers.batch_normalization(dconv_layer_7,
+                                                  name="new_dconv_layer_7_normalization",
+                                                  training = is_training)
 
     filters = 512 if USE_ORIGINAL_NUM_FILTERS else num_classes
     conv_layer_4 = tf.layers.conv2d(vgg_layer4_out, filters, 1, strides=(1, 1),
@@ -96,6 +99,9 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
                                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
                                                name='new_dconf_layer_4')
+    dconv_layer_4 = tf.layers.batch_normalization(dconv_layer_4,
+                                                  name="new_dconv_layer_4_normalization",
+                                                  training = is_training)
 
     filters = 256 if USE_ORIGINAL_NUM_FILTERS else num_classes
     conv_layer_3 = tf.layers.conv2d(vgg_layer3_out, filters, 1, strides=(1, 1),
@@ -113,25 +119,40 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
                                                kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
                                                kernel_regularizer=tf.contrib.layers.l2_regularizer(1e-3),
                                                name='new_dconf_layer_3')
+    dconv_layer_3 = tf.layers.batch_normalization(dconv_layer_3,
+                                                  name="new_dconv_layer_3_normalization",
+                                                  training = is_training)
 
     # Debug info
     dconv_layer_3 = tf.Print(dconv_layer_3, [tf.shape(vgg_layer7_out)],
                              message='vgg_layer7_out = ', summarize = 10, first_n = 3)
+    dconv_layer_3 = tf.Print(dconv_layer_3, [vgg_layer7_out],
+                             message='vgg_layer7_out = ', summarize = 10, first_n = 3)
     dconv_layer_3 = tf.Print(dconv_layer_3, [tf.shape(dconv_layer_7)],
+                             message='dconv_layer_7 = ', summarize = 10, first_n = 3)
+    dconv_layer_3 = tf.Print(dconv_layer_3, [dconv_layer_7],
                              message='dconv_layer_7 = ', summarize = 10, first_n = 3)
 
     dconv_layer_3 = tf.Print(dconv_layer_3, [tf.shape(vgg_layer4_out)],
                              message='vgg_layer4_out = ', summarize = 10, first_n = 3)
+    dconv_layer_3 = tf.Print(dconv_layer_3, [vgg_layer4_out],
+                             message='vgg_layer4_out = ', summarize = 10, first_n = 3)
     dconv_layer_3 = tf.Print(dconv_layer_3, [tf.shape(dconv_layer_4)],
+                             message='dconv_layer_4 = ', summarize = 10, first_n = 3)
+    dconv_layer_3 = tf.Print(dconv_layer_3, [dconv_layer_4],
                              message='dconv_layer_4 = ', summarize = 10, first_n = 3)
 
     dconv_layer_3 = tf.Print(dconv_layer_3, [tf.shape(vgg_layer3_out)],
                              message='vgg_layer3_out = ', summarize = 10, first_n = 3)
+    dconv_layer_3 = tf.Print(dconv_layer_3, [vgg_layer3_out],
+                             message='vgg_layer3_out = ', summarize = 10, first_n = 3)
     dconv_layer_3 = tf.Print(dconv_layer_3, [tf.shape(dconv_layer_3)],
+                             message='dconf_layer_3 = ', summarize = 10, first_n = 3)
+    dconv_layer_3 = tf.Print(dconv_layer_3, [dconv_layer_3],
                              message='dconf_layer_3 = ', summarize = 10, first_n = 3)
 
     return dconv_layer_3
-tests.test_layers(layers)
+#tests.test_layers(layers)
 
 
 def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
@@ -145,14 +166,24 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     """
     logits = tf.reshape(nn_last_layer, (-1, num_classes), name='new_logits')
 
-    #labels = tf.reshape(correct_label, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+
+    logits = tf.Print(logits, [logits], message='logits: ', summarize = 100, first_n = 3)
+    labels = tf.Print(labels, [labels], message='labels: ', summarize = 100, first_n = 3)
+    #labels = tf.cast(labels, tf.float32)
+
+    #cross_entropy_loss = tf.reduce_mean(
+    #    tf.nn.softmax_cross_entropy_with_logits(logits=nn_last_layer, labels=correct_label))
 
     cross_entropy_loss = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(logits=nn_last_layer, labels=correct_label))
+        tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+
+    is_correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(tf.cast(labels, tf.float32), 1))
+    accuracy_op = tf.reduce_mean(tf.cast(is_correct_prediction, tf.float32), name="accuracy_op")
 
     reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
     reg_constant = 0.01
-    loss_operation = tf.reduce_mean(cross_entropy_loss +  reg_constant * sum(reg_losses))
+    loss_operation = tf.reduce_mean(cross_entropy_loss + reg_constant * sum(reg_losses))
 
     optimizer = tf.train.AdamOptimizer(learning_rate = learning_rate)
     #optimizer = tf.train.AdagradOptimizer(learning_rate = learning_rate)
@@ -160,8 +191,8 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     # https://discussions.udacity.com/t/using-transfer-learning/487140?u=subodh.malgonde
     train_op = optimizer.minimize(loss_operation)
 
-    return logits, train_op, loss_operation
-tests.test_optimize(optimize)
+    return logits, train_op, loss_operation, accuracy_op
+#tests.test_optimize(optimize)
 
 
 def print_evaluate_results(im_softmax, labels):
@@ -180,10 +211,16 @@ def print_evaluate_results(im_softmax, labels):
     correct_negative = 0
     incorrect_positive_pred = 0
     incorrect_negative_pred = 0
+    postive = 0
+    negative = 0
     for i in range(shape[0]):
       for j in range(shape[1]):
-        inference = softmax[i][j] >= 0.5
+        inference = softmax[i][j][0] < softmax[i][j][1]
         ground_truth = label[i][j][1]
+        if ground_truth:
+            postive += 1
+        else:
+            negative += 1
         if inference == ground_truth:
           if inference:
             correct_positive += 1
@@ -194,9 +231,11 @@ def print_evaluate_results(im_softmax, labels):
             incorrect_positive_pred += 1
           else:
             incorrect_negative_pred += 1
-    print("Per: correct=({}, {}), incorrect=({}, {})".format(
+    print("Per: correct=({}, {}), incorrect=({}, {}), ({}, {}) -- {:.2f}%".format(
       correct_positive, correct_negative,
-      incorrect_positive_pred, incorrect_negative_pred))
+      incorrect_positive_pred, incorrect_negative_pred,
+      postive, negative,
+      100. * (correct_positive + correct_negative) / (postive + negative)))
 
     total_correct_positive += correct_positive
     total_correct_negative += correct_negative
@@ -206,15 +245,23 @@ def print_evaluate_results(im_softmax, labels):
         total_correct_positive, total_correct_negative,
         total_incorrect_positive_pred, total_incorrect_negative_pred))
 
-def evaluate_accurancy(sess, logits, input_image, keep_prob, valid_images, valid_labels):
-    im_softmax = sess.run(tf.nn.softmax(logits), {keep_prob: 1.0, input_image: valid_images})
+def evaluate_accurancy(sess,
+                       logits, accuracy_op,
+                       input_image, correct_label, keep_prob, is_training,
+                       valid_images, valid_labels):
+    im_softmax, accuracy = sess.run([tf.nn.softmax(logits), accuracy_op],
+                                    {keep_prob: 1.0,
+                                     input_image: valid_images,
+                                     correct_label: valid_labels,
+                                     is_training: False})
     num_images = valid_labels.shape[0]
-    im_softmax = im_softmax[:, 1].reshape(num_images, IMAGE_SHAPE[0], IMAGE_SHAPE[1])
+    im_softmax = im_softmax.reshape(num_images, IMAGE_SHAPE[0], IMAGE_SHAPE[1], 2)
+    print('accuracy = ', accuracy)
     print_evaluate_results(im_softmax, valid_labels)
 
 
-def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, logits, loss_op,
-             input_image, correct_label, keep_prob, learning_rate):
+def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, logits, loss_op, accuracy_op,
+             input_image, correct_label, keep_prob, learning_rate, is_training):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -239,10 +286,14 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, logits, loss_op
             _, loss = sess.run([train_op, loss_op],
                                 feed_dict = {input_image: images,
                                              correct_label: labels,
-                                             keep_prob: 0.5,
-                                             learning_rate: FLAGS.learning_rate})
+                                             keep_prob: 1.0,
+                                             learning_rate: FLAGS.learning_rate,
+                                             is_training: True})
             print("Loss {:.3f}".format(loss))
-            evaluate_accurancy(sess, logits, input_image, keep_prob, valid_images, valid_labels)
+            evaluate_accurancy(sess,
+                               logits, accuracy_op,
+                               input_image, correct_label, keep_prob, is_training,
+                               valid_images, valid_labels)
     print('Finished train_nn')
 #tests.test_train_nn(train_nn)
 
@@ -284,8 +335,10 @@ def run():
         layer4_out = tf.stop_gradient(layer4_out)
         layer3_out = tf.stop_gradient(layer3_out)
 
-        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes)
-        logits, train_op, loss_op = optimize(layer_output, correct_label, learning_rate, num_classes)
+        is_training = tf.placeholder(tf.bool, name="is_training")
+        layer_output = layers(layer3_out, layer4_out, layer7_out, num_classes, is_training)
+        logits, train_op, loss_op, accuracy_op = optimize(
+            layer_output, correct_label, learning_rate, num_classes)
 
         # Train NN using the train_nn function
         sess.run(tf.global_variables_initializer())
@@ -293,7 +346,7 @@ def run():
         #sess.run(my_variable_initializers)
 
         train_nn(sess, FLAGS.epochs, FLAGS.batch_size, get_batches_fn, train_op, logits,
-                 loss_op, input_image, correct_label, keep_prob, learning_rate)
+                 loss_op, accuracy_op, input_image, correct_label, keep_prob, learning_rate, is_training)
 
         builder.add_meta_graph_and_variables(sess, ['test'])
 
